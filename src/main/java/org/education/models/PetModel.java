ee77c7832dd9e4afaa126fd0b4b2027aa356f921
Nazarii Kupchenko <nazarii.kupchenko@gmail.com>
2022-01-17 20:55:18 +0200

    add lombok annotations to models

--- src/main/java/org/education/models/PetModel.java
+++ src/main/java/org/education/models/PetModel.java
@@ -1,5 +1,12 @@
 package org.education.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class PetModel {
     public int id;
     public CategoryModel category;
@@ -7,61 +14,4 @@ public class PetModel {
     public String[] photoUrls;
     public TagModel tagModel;
     public String status;
-
-    public PetModel(int id, CategoryModel category, String name, String[] photoUrls, TagModel tagModel, String status) {
-        this.id = id;
-        this.category = category;
-        this.name = name;
-        this.photoUrls = photoUrls;
-        this.tagModel = tagModel;
-        this.status = status;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public CategoryModel getCategory() {
-        return category;
-    }
-
-    public void setCategory(CategoryModel category) {
-        this.category = category;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String[] getPhotoUrls() {
-        return photoUrls;
-    }
-
-    public void setPhotoUrls(String[] photoUrls) {
-        this.photoUrls = photoUrls;
-    }
-
-    public TagModel getTagModel() {
-        return tagModel;
-    }
-
-    public void setTagModel(TagModel tagModel) {
-        this.tagModel = tagModel;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
 }
--- src/main/java/org/education/models/TagModel.java
+++ src/main/java/org/education/models/TagModel.java
@@ -1,27 +1,13 @@
 package org.education.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class TagModel {
     public int id;
     public String name;
-
-    public TagModel(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
